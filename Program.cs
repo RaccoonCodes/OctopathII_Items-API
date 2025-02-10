e11b700e26f1ccc6653118ab5ad3ce7e6bab0410
RaccoonCodes <61885457+RaccoonCodes@users.noreply.github.com>
2025-02-09 21:21:18 -0600

    Added Packages

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
 
 app.Run();
+
+/* Packages
+ * Microsoft.EntityFrameworkCore version 8.0.11
+ * Microsoft.EntityFrameworkCore.Tools version 8.0.11
+ * Microsoft.EntityFrameworkCore.SqlServer version 8.0.11
+ * Swashbuckle.AspNetCore --version 7.2.0
+ * 
+ */
